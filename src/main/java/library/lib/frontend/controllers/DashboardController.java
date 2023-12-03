9b495f498d6d3a2ac54b9969ee0ae2c10b094130
Marcin Bereta <412994@student.agh.edu.pl>
2023-12-03 14:56:19 +0100

    Added users list on the dashboard

--- src/main/java/library/lib/frontend/controllers/DashboardController.java
+++ src/main/java/library/lib/frontend/controllers/DashboardController.java
@@ -4,16 +4,25 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.text.Text;
 import library.lib.backend.models.Member;
+import library.lib.backend.services.MemberService;
 import library.lib.frontend.state.UserState;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 @Component
 public class DashboardController extends BaseController {
 
     @FXML
     private Text helloText;
+
+    @FXML
+    private Text userList;
+
+    @Autowired
+    MemberService service;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -24,6 +33,14 @@ public class DashboardController extends BaseController {
         Member loggedInUser = UserState.getInstance().getLoggedInUser();
         if (loggedInUser != null) {
             helloText.setText("Hello " + loggedInUser.getName() + "!");
+            List<Member> members = service.getLatestMembers(5);
+            System.out.println(members);
+            String text = "";
+            for(Member member: members){
+                text += member.getName() + "\n";
+            }
+            userList.setText(text);
+
         }
     }
 
