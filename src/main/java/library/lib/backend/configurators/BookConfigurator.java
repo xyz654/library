8b0e00bfb82823bfd3b5d8c23d4910de82871958
Marcin Bereta <412994@student.agh.edu.pl>
2023-12-20 13:47:01 +0100

    Removed whitespace in BookConfigurator

--- src/main/java/library/lib/backend/configurators/BookConfigurator.java
+++ src/main/java/library/lib/backend/configurators/BookConfigurator.java
@@ -11,7 +11,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class BookConfigurator {
     @Autowired
     CommandLineRunner commandLineRunner(AuthorRepository authorRepository, BookRepository bookRepository) {
