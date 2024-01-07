571f84f4e4eac91817972f9176b3110ad876fa7d
xyz654 <101123142+xyz654@users.noreply.github.com>
2024-01-07 13:04:57 +0100

    remove unused imports

--- src/main/java/library/lib/frontend/controllers/NavbarController.java
+++ src/main/java/library/lib/frontend/controllers/NavbarController.java
@@ -2,8 +2,6 @@ package library.lib.frontend.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
